eb530f92871b8e7488d99afbadc282265372c933
ChristianGeng <christian.c.geng@gmail.com>
2018-05-16 11:12:57 +0200

    Create setup-speedbar.el

---
+++ el/setup-speedbar.el
@@ -0,0 +1,22 @@
+(require 'sr-speedbar)
+
+;; autorefresh it                                                                                                                                                                                                                                                                                                                                                          
+(setq sr-speedbar-auto-refresh t)
+
+;; and/or turn it on and off later with the function sr-speedbar-refresh-toggle.                                                                                                                                                                                                                                                                                           
+;; The defaults it (and speedbar) comes with are a bit odd though - it also doesn't show files that it doesn't recognize, uses images for buttons, and is set on the right side - so you can set these if you'd like -                                                                                                                                                     
+
+(setq speedbar-show-unknown-files nil) ; show all files                                                                                                                                                                                                                                                                                                                    
+(setq speedbar-use-images nil) ; use text for buttons                                                                                                                                                                                                                                                                                                                      
+
+(setq sr-speedbar-right-side t) ; put on right side                                                                                                                                                                                                                                                                                                                        
+
+;; improve speedbars behavior                                                                                                                                                                                                                                                                                                                                              
+;; see https://stackoverflow.com/questions/20265938/emacs-speedbar-does-not-show-all-classs-methods                                                                                                                                                                                                                                                                        
+                                                                                                                                                                                                                                                                                                                                                                           
+(global-ede-mode 1)
+(require 'semantic/sb)
+(semantic-mode 1)
+
+(provide 'setup-speedbar)
+
