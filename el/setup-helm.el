1480ac8ce4aac869066dc2f9707c2cb32b3a9c16
ChristianGeng <christian.c.geng@gmail.com>
2018-05-17 09:17:00 +0200

    Update setup-helm.el

--- el/setup-helm.el
+++ el/setup-helm.el
@@ -32,7 +32,16 @@
 (helm-autoresize-mode 1)
 
 (helm-mode 1)
+(setq helm-M-x-fuzzy-match t) ;; optional fuzzy matching for helm-M-x
 
+(global-set-key (kbd "M-x") 'helm-M-x)
 
+;;(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-M-y") 'helm-show-kill-ring)
+
+(global-set-key (kbd "C-x b") 'helm-mini)
+
+(setq helm-buffers-fuzzy-matching t
+      helm-recentf-fuzzy-match    t)
 
 (provide 'setup-helm)
