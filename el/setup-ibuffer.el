26f880af975ea63cdd37b5a8e9b2ba9233aa2763
ChristianGeng <christian.c.geng@gmail.com>
2018-05-17 20:21:38 +0200

    Create setup-ibuffer.el

---
+++ el/setup-ibuffer.el
@@ -0,0 +1,14 @@
+;; nearly all of this is the default layout
+(setq ibuffer-formats 
+      '((mark modified read-only " "
+              (name 40 40 :left :elide) ; change: 30s were originally 18s
+              " "
+              (size 9 -1 :right)
+              " "
+              (mode 16 16 :left :elide)
+              " " filename-and-process)
+        (mark " "
+              (name 16 -1)
+              " " filename)))
+              
+ (provide 'setup-ibuffer)
