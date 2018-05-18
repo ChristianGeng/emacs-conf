9a5b48607c1d3b84776902019c279c30a80e3824
ChristianGeng <christian.c.geng@gmail.com>
2018-05-18 16:38:52 +0200

    Update setup-ibuffer.el

--- el/setup-ibuffer.el
+++ el/setup-ibuffer.el
@@ -1,4 +1,20 @@
 ;; nearly all of this is the default layout
+;; interesting example conf at https://github.com/purcell/emacs.d/blob/master/lisp/init-ibuffer.el
+;; 
+
+
+(after-load 'ibuffer
+  ;; Use human readable Size column instead of original one
+  (define-ibuffer-column size-h
+    (:name "Size" :inline t)
+    (cond
+     ((> (buffer-size) 1000000) (format "%7.1fM" (/ (buffer-size) 1000000.0)))
+     ((> (buffer-size) 1000) (format "%7.1fk" (/ (buffer-size) 1000.0)))
+     (t (format "%8d" (buffer-size))))))
+
+
+
+
 (setq ibuffer-formats 
       '((mark modified read-only " "
               (name 40 40 :left :elide) ; change: 30s were originally 18s
