ff44180d844bc1740153dc02bb44dbddab2721c9
Christian Geng <cgn@norcom.de>
2019-02-26 13:46:35 +0100

    add function to copy the current file name to the clipboard

--- el/cglispfuncs.el
+++ el/cglispfuncs.el
@@ -231,6 +231,21 @@
  (shell-command "source cglib; bash getgooglecal.sh &>/dev/null"))
 
 
+;; shamelessly adopted from here
+;; https://emacsredux.com/blog/2013/03/27/copy-filename-to-the-clipboard/
+;; this uses the absolute path
+(defun cg-copy-current-filename-to-clipboard ()
+  "Copy the current buffer file name to the clipboard."
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (kill-new filename)
+      (message "Copied buffer file name '%s' to the clipboard." filename))))
+
+
+
 
 
 
