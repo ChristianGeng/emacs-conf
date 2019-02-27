11457efc4e74f31919864a40a8cc54a6cb6b3efb
Christian Geng <cgn@norcom.de>
2019-02-27 10:22:16 +0100

    remove bug in projectile setup

--- el/setup-projectile.el
+++ el/setup-projectile.el
@@ -4,12 +4,12 @@
 ;;    :bind-keymap (("C-c p" . projectile-command-map)))
 
 (use-package projectile
-   :ensure tg
+   :ensure t
    :bind-keymap (("s-c" . projectile-command-map)))
 
 
-(add-hook 'clojure-mode-hook 'projectile-mode)
-(add-hook 'ruby-mode-hook 'projectile-mode)
+;; (add-hook 'clojure-mode-hook 'projectile-mode)
+;; (add-hook 'ruby-mode-hook 'projectile-mode)
 
 
 (provide 'setup-projectile)
