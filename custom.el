3289679c9d69df153f9c5701efe051e575cf8d3b
ChristianGeng <christian.c.geng@gmail.com>
2019-05-23 01:30:21 +0200

    add rng for nxml after https://github.com/skybert/my-little-friends/

--- custom.el
+++ custom.el
@@ -9,11 +9,16 @@
  '(custom-safe-themes
    (quote
     ("d61f6c49e5db58533d4543e33203fd1c41a316eddb0b18a44e0ce428da86ef98" "d2e9c7e31e574bf38f4b0fb927aaff20c1e5f92f72001102758005e53d77b8c9" "10461a3c8ca61c52dfbbdedd974319b7f7fd720b091996481c8fb1dded6c6116" "fe666e5ac37c2dfcf80074e88b9252c71a22b6f5d2f566df9a7aa4f9bea55ef8" "b59d7adea7873d58160d368d42828e7ac670340f11f36f67fa8071dbf957236a" "64ca5a1381fa96cb86fd6c6b4d75b66dc9c4e0fc1288ee7d914ab8d2638e23a9" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "2642a1b7f53b9bb34c7f1e032d2098c852811ec2881eec2dc8cc07be004e45a0" "251348dcb797a6ea63bbfe3be4951728e085ac08eee83def071e4d2e3211acc3" "946e871c780b159c4bb9f580537e5d2f7dba1411143194447604ecbaf01bd90c" "2540689fd0bc5d74c4682764ff6c94057ba8061a98be5dd21116bf7bf301acfb" "d8dc153c58354d612b2576fea87fe676a3a5d43bcc71170c62ddde4a1ad9e1fb" default)))
+ '(eclim-eclipse-dirs (quote ("/opt/eclipse-jee-photon/")))
+ '(eclim-executable "/opt/eclipse-jee-photon/plugins/org.eclim_2.8.0/bin/eclim")
  '(markdown-command "pandoc")
  '(org-agenda-files (quote ("~/.emacs.d/agenda.org")))
  '(package-selected-packages
    (quote
     (treemacs company-emacs-eclim eclim dumb-jump plantuml-mode company-shell yafolding ox-rst js-import js-auto-beautify indium xref-js2 web-mode-edit-element web-completion-data web-beautify rainbow-delimiters doom-modeline doom-themes popup-imenu sphinx-doc anti-zenburn-theme dockerfile-mode diff-hl git-wip-timemachine js-comint wttrin jquery-doc multi-web-mode web-mode java-snippets buster-snippets angular-snippets company-tern js2-refactor flycheck jenkins-watch jenkins butler js2-mode org-plus-contrib ecb tj3-mode ag groovy-mode color-theme-solarized atom-dark-theme airline-themes helm-ag wgrep-helm color-theme-github cider-eval-sexp-fu gited ob-async ob-sql-mode ob-ipython ecb-snapshot swoop mvn mvn-help projectile-extras maven-test-mode jdee visual-regexp projectile-speedbar ac-octave magit-gh-pulls magit-find-file magit yaml-mode cdlatex apt-sources-list wgrep py-isort jedi-core jedi elpygen common-lisp-snippets yasnippet-classic-snippets markdown-mode markdown-preview-eww markdown-toc ample-theme afternoon-theme auctex dired-atool openwith dired-fdclone dired-explorer yasnippet-snippets virtualenv use-package undo-tree tagedit sr-speedbar spacemacs-theme smooth-scrolling smex realgud python-x python-mode python-django pyenv-mode-auto py-autopep8 pungi pony-mode pippel pip-requirements paredit markdown-preview-mode markdown-mode+ magit-svn jump-to-line isend-mode image-dired+ helm-swoop flymd fill-column-indicator fancy-battery elpy ein egg django-theme django-snippets django-mode django-manage dired-sidebar dired-rsync dired-quick-sort dired-narrow dired-k dired-du dired-details dired-collapse diminish cyberpunk-theme browse-kill-ring bash-completion all-the-icons-dired abyss-theme)))
+ '(rng-schema-locating-files
+   (quote
+    ("schemas.xml" "/usr/share/emacs/25.2/etc/schema/schemas.xml" "~/.emacs.d/schemas.xml")))
  '(yas-prompt-functions
    (quote
     (yas-completing-prompt yas-dropdown-prompt yas-maybe-ido-prompt yas-no-prompt))))
--- el/setup-nxml.el
+++ el/setup-nxml.el
@@ -1,10 +1,63 @@
 ;;; reformat nxml mode
 
+;; https://www.emacswiki.org/emacs/NxmlModeSchemas
+;; emacs variable rng-schema-locating-files
+;; 
+
+
 (defun local-nxml-hook ()
   (setq indent-tabs-mode t)
   (setq nxml-child-indent 4)
+  (setq sgml-set-face t)
+  ;; validiting with xmllint, so XML decl. not needed
+  (setq sgml-declaration nil)
+  ;; invoke xmllint for external validation
+  (setq sgml-validation-command "xmllint --noout --postvalid %s %s")
+  (setq popcmp-group-alternatives nil)
+  (setq rng-validate-delay 3)
+  (setq nxml-slash-auto-complete-flag t)
+  (setq nxml-child-indent 2)
+  (setq nxml-attribute-indent 2)
+  (define-key nxml-mode-map "\C-c\C-i" 'yas/expand)
+  (rng-nxml-mode-init)
   )
 
 (add-hook 'nxml-mode-hook '(lambda () (local-nxml-hook)))
+
+
+  (push "~/.emacs.d/schemas.xml" rng-schema-locating-files-default)
+
+(push (cons (concat "\\." (regexp-opt '("svg" "x3d") t)
+                      "\\'") 'nxml-mode) auto-mode-alist)
+
+
+
+;; Showing the current xpath in the echo area.
+;;
+;; Based on code snippet
+;; from http://www.emacswiki.org/emacs/NxmlMode
+(defun nxml-current-xpath ()
+  "Displays the XPATH of where you currently are in the  XML docment."
+  (interactive)
+  (let ((path nil))
+    (save-excursion
+      (save-restriction
+        (widen)
+        ;; Doesn't error if point is at beginning of buffer
+        (while (and (< (point-min) (point))
+                    (condition-case nil
+                        (progn
+                          (nxml-backward-up-element) ; always returns nil
+                          t)
+                      (error nil)))
+          (setq path
+                (cons
+                 (if (xmltok-start-tag-prefix)
+                     (concat (xmltok-start-tag-prefix) ":"
+                             (xmltok-start-tag-local-name))
+                   (xmltok-start-tag-local-name))
+                 path)))
+        (if (called-interactively-p t)
+            (message (concat "/" (mapconcat 'identity path "/"))))))))
 
 (provide 'setup-nxml)
