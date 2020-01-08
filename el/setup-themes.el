2b2b7244d3c09a7fd97fd4dae8e99dde71c003af
ChristianGeng <christian.c.geng@gmail.com>
2020-01-08 01:58:11 +0100

    theme cycle bug gerichtet

--- el/setup-themes.el
+++ el/setup-themes.el
@@ -1,7 +1,7 @@
 
 (load-theme 'misterioso)
 
-(defcustom cycle-themes '(doom-solarized-dark doom-solarized-light atom-dark intellij idea-darkula subatomic solarized subatomic-theme)
+(defcustom cycle-themes '(doom-solarized-dark doom-solarized-light atom-dark intellij idea-darkula subatomic solarized)
   "List of themes through which one can switch easily by calling
 `cycle-themes' repeatedly. The first element is loaded at startup."
   :type '(repeat symbol))
