79d52f9a27a5a612ca6fe6d606dde772c7b9258b
Christian Geng <cgeng@audeering.com>
2020-06-10 07:26:49 +0200

    add flycheck setup

---
+++ el/setup-flycheck-mode.el
@@ -0,0 +1,64 @@
+;;; package --- Flycheck Configuration
+;;; Commentary:
+;;;
+;;; Wahrscheinlich ist der flycheck mode bereits in anderen Modes
+;;; global gesetzt worden:
+;;; el/setup-web-mode.el
+;;; 60:40:(add-hook 'after-init-hook #'global-flycheck-mode)
+;;; 21:32:(add-hook 'prog-mode-hook #'flycheck-mode)
+;;; File: el/setup-ensime.el
+;;; 21:32:(add-hook 'prog-mode-hook #'flycheck-mode)
+;;;
+;;; (global-flycheck-mode) ;; enable flycheck globally
+;;; https://stackoverflow.com/questions/37720869/emacs-how-do-i-set-flycheck-to-python-3
+
+;;; Code:
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+
+;; das hier scheint für die package liste zu tun
+;; (defcustom dse/package-menu/package-column-width 10
+;;   "Column width of package name in list-packages menu."
+;;   :type 'number :group 'package)
+;; (defcustom dse/package-menu/archive-column-width 12
+;;   "Column width of archive name in list-packages menu."
+;;   :type 'number :group 'package)
+;; (defun dse/package-menu/fix-column-widths ()
+;;   (let ((tlf (append tabulated-list-format nil)))
+;;     (setf (cadr (assoc "Package" tlf)) dse/package-menu/package-column-width)
+;;     (setf (cadr (assoc "Archive" tlf)) dse/package-menu/archive-column-width)
+;;     (setq tabulated-list-format (vconcat tlf))))
+
+;; (add-hook 'package-menu-mode-hook #'dse/package-menu/fix-column-widths)
+
+
+;;; https://github.com/flycheck/flycheck/issues/1101
+
+ ;; (add-hook 'flycheck-error-list-mode-hook
+ ;;            (lambda ()
+ ;;              (setq tabulated-list-format '[("Line" 5 flycheck-error-list-entry-< :right-align t)
+ ;;                                            ("Col" 3 nil :right-align t)
+ ;;                                            ("Level" 8 flycheck-error-list-entry-level-<)
+ ;;                                            ("ID" 20 t)
+ ;;                                            (#("Message (Checker)" 0 9
+ ;;                                               (face default)
+ ;;                                               9 16
+ ;;                                               (face flycheck-error-list-checker-name)
+ ;;                                               16 17
+ ;;                                               (face default))
+ ;;                                             0 t)])))
+
+;;; Modify Error List Display to be at the bottom rather than a full buffer on the side
+;;; see https://www.flycheck.org/en/latest/user/error-list.html
+(add-to-list 'display-buffer-alist
+             `(,(rx bos "*Flycheck errors*" eos)
+              (display-buffer-reuse-window
+               display-buffer-in-side-window)
+              (side            . bottom)
+              (reusable-frames . visible)
+              (window-height   . 0.33)))
+
+
+(provide 'setup-flycheck-mode)
+;;; setup-flycheck-mode.el ends here
