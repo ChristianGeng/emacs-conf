5d99b4a07d83c2ca0f11b5926db065be5b2b021a
Christian Geng <cgeng@audeering.com>
2020-10-25 09:22:03 +0100

    add editorconfig setup

---
+++ el/setup-editorconfig.el
@@ -0,0 +1,39 @@
+;;; name.el --- summary -*- lexical-binding: t -*-
+
+;; Author: CGeng
+;; Maintainer: CGeng
+;; Version: version
+;; Package-Requires: (dependencies)
+;; Homepage: homepage
+;; Keywords: keywords
+
+
+;; This file is not part of GNU Emacs
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; commentary
+
+;;; Code:
+
+(use-package editorconfig
+  :ensure t
+  :config
+  (editorconfig-mode 1))
+
+(provide 'setup-editorconfig)
+;;; setup-editorconfig.el ends here
