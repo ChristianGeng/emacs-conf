13def83e122f278c07ec1c46d641ebd26acceda0
ChristianGeng <christian.c.geng@gmail.com>
2021-08-08 22:39:52 +0200

    First Step Towards Tangling

--- el/setup-plantuml.el
+++ el/setup-plantuml.el
@@ -4,7 +4,7 @@
 ;; plant_uml support
 ;; https://plantuml.com/de/emacs
 ;;; Code:
-
+(setq plantuml-jar-path "/home/audeering.local/cgeng/bin/plantuml.jar")
 (setq org-plantuml-jar-path (expand-file-name "~/.emacs.d/plant_uml/plantuml.jar"))
 (add-to-list 'org-src-lang-modes '("plantuml" . plantuml))
 (setq plantuml-default-exec-mode 'jar)
--- el/setup-themes.el
+++ el/setup-themes.el
@@ -1,7 +1,7 @@
 
 ;; (load-theme 'fantom)
 (load-theme 'moe-light)
-(defcustom cycle-themes '(fantom material material-light doom-solarized-dark doom-solarized-light atom-dark intellij idea-darkula subatomic solarized moe-light)
+(defcustom cycle-themes '(material material-light doom-solarized-dark doom-solarized-light atom-dark intellij idea-darkula subatomic solarized moe-light)
   "List of themes through which one can switch easily by calling
 `cycle-themes' repeatedly. The first element is loaded at startup."
   :type '(repeat symbol))
