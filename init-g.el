6ea487b513822109aaa3e1c52385000085055b2c
ChristianGeng <christian.c.geng@gmail.com>
2022-12-02 08:05:41 +0100

    org roam.

--- init-g.el
+++ init-g.el
@@ -388,6 +388,27 @@
 ;; (setq sqlformat-command 'pgformatter)
 ;; (setq sqlformat-args '("-s2" "-g"))
 
+(use-package org-roam
+  :ensure t
+  :init
+  (setq org-roam-v2-ack t)
+  :custom
+  (org-roam-directory "~/RoamNotes")
+  (org-roam-completion-everywhere t)
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n i" . org-roam-node-insert)
+         :map org-mode-map
+         ("C-M-i" . completion-at-point)
+         :map org-roam-dailies-map
+         ("Y" . org-roam-dailies-capture-yesterday)
+         ("T" . org-roam-dailies-capture-tomorrow))
+  :bind-keymap
+  ("C-c n d" . org-roam-dailies-map)
+  :config
+  (require 'org-roam-dailies) ;; Ensure the keymap is available
+  (org-roam-db-autosync-mode))
+
 (require 'setup-org) ;; organizer todo notes etc
 
 ;; (require 'setup-speedbar)
