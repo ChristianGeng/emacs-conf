210064f7efca0e83bddf237cfb8f5efdb5d6dde2
cgeng <cgeng@audeering.com>
2025-09-09 13:50:24 +0200

    Delete defuns from this repo.

--- defuns/nix-defuns.el
+++
@@ -1,9 +0,0 @@
-;;; nix helper
-
-(defun find-corresponding-nix-hash (file)
-  "Given some file or directory which ultimately lies in the nix
-store. Return the corresponding nix hash."
-  (when file
-   (let ((truename (file-truename file)))
-     (when (string-match "/nix/store/\\([^/-]+\\)-" truename)
-       (match-string 1 truename)))))
